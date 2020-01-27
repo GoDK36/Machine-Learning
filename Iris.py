e2f312270ae7d86cc7e502e2a8770af801168645
GoDK36 <siinwoo0306@gmail.com>
2020-01-27 13:19:09 +0900

    간단한 분류 알고리즘

---
+++ Iris.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import Perceptron
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/''machine-learning-databases/iris/iris.data',
+                 header=None)
+
+print(df.tail())
+
+# setosa와 versicolor를 선택한다
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# 꽃받침 길이와 꽃잎 길이를 추출한다.
+X = df.iloc[0:100,[0,2]].values
+
+# 산점도를 그린다.
+plt.scatter(X[:50,0], X[:50,1],
+color ='red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100,0], X[50:100,1],
+color ='blue', marker = 'x', label = 'versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1),
+ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of errors')
+plt.show()
