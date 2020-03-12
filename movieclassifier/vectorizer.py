59782a937fbb5ea20c6bea86ed183f2b18109eb3
GoDK36 <siinwoo0306@gmail.com>
2020-03-12 15:39:01 +0900

    pickle

---
+++ movieclassifier/vectorizer.py
@@ -0,0 +1,26 @@
+###HashingVectorizer 객체를 임포트 할 수 있기 위한 코드###
+
+from sklearn.feature_extraction.text import HashingVectorizer
+import re
+import os
+import pickle
+
+
+cur_dir = os.path.dirname(__file__)
+stop = pickle.load(open(
+                    os.path.join(cur_dir,
+                    'pkl_objects',
+                    'stopwords.pkl'), 'rb'))
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) \
+                    + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+vect = HashingVectorizer(decode_error='ignore',
+                        n_features=2**21,
+                        preprocessor=None,
+                        tokenizer=tokenizer)
